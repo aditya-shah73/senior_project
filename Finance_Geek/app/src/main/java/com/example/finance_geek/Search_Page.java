3b73011338c991f3c5a38312c60b289a04a693f5
davidpham117 <davidpham117@gmail.com>
2016-12-08 04:12:33 +0000

    readding google places part 2

--- Finance_Geek/app/src/main/java/com/example/finance_geek/Search_Page.java
+++ Finance_Geek/app/src/main/java/com/example/finance_geek/Search_Page.java
@@ -2,6 +2,7 @@ package com.example.finance_geek;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,14 +11,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.*;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.view.View;
+
 
 public class Search_Page extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements OnConnectionFailedListener, NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleApiClient mGoogleApiClient;
 
@@ -27,6 +36,8 @@ public class Search_Page extends AppCompatActivity
         setContentView(R.layout.activity_search__page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final Button placesButton = (Button) findViewById(R.id.placesButton);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -44,16 +55,20 @@ public class Search_Page extends AppCompatActivity
                 .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
                 .build();
 
-        int PLACE_PICKER_REQUEST = 1;
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
-        try {
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException e) {
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }
+        // Google Place Picker button listener
+        placesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int PLACE_PICKER_REQUEST = 1;
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(Search_Page.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -117,5 +132,10 @@ public class Search_Page extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
