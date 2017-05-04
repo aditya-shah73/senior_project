6780f098bd8b2eb188903d1d81d5bf523382b795
aditya7395 <aditya7395@gmail.com>
2017-05-04 16:33:00 -0700

    Revert "Removed unwanted log"
    This reverts commit c7c6dd0e257b8236e5d2bf97bd6f86b219f9965a.

--- Finance_Geek/app/src/main/java/com/example/finance_geek/HomePage.java
+++ Finance_Geek/app/src/main/java/com/example/finance_geek/HomePage.java
@@ -185,6 +185,7 @@ public class HomePage extends AppCompatActivity
                         Log.v("DATE AT TOP: ", date.getText().toString());
                         if(value.date.equals(date.getText().toString())) {
                             adapter.add(value);
+                            Log.v("test", "test");
                         }
                     }
 
